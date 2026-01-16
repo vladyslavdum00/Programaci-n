119aa22f65d2213a9e6b25eeea7db7cbb21f228a
Vladyslav Dumynsky <vladum@alu.edu.gva.es>
2026-01-16 12:15:07 +0100

    cambios de primera practica de OOP

--- UD_5/practica_A/Persona.java
+++ UD_5/practica_A/Persona.java
@@ -1,28 +1,54 @@
 public class Persona {
-    public String dni;
-    public String nombre;
-    public String apellido;
-    public int edad;
+    private String dni;
+    private String nombre;
+    private String apellido;
+    private int edad;
 
     public Persona() {};
     public Persona(String dni, String nombre, String apellido, int edad) {
+        setDni(dni);
+        setNombre(nombre);
+        setApellido(apellido);
+        setEdad(edad);
+    }
+
+    public String getDni() {
+        return dni;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public String getApellido() {
+        return apellido;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setDni(String dni) {
         this.dni = dni;
+    }
+    public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+    public void setApellido(String apellido) {
         this.apellido = apellido;
+    }
+    public void setEdad(int edad) {
         this.edad = edad;
     }
-
     public void muestraDatos(){
-        System.out.println("DNI: " + this.dni);
-        System.out.println("Nombre: " + this.nombre);
+        System.out.println(getDni() + " " + getNombre() + " " + getApellido() + " " + getEdad());
     }
 
-    public void es_mayor_edad(){
-        if(this.edad >= 18){
+    public boolean es_mayor_edad(){
+        if(getEdad() >= 18){
             System.out.println("Es mayor de edad:");
+            return  true;
 
         }else{
-            System.out.println("No es mayor de edad:");
+            System.out.println("Es mayor de edad:");
+            return false;
 
         }
     }
