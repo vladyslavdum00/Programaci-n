a69c0bb23ab817aaffa09024c7f9cde4c5703c19
Vladyslav <vladum@alu.edu.gva.es>
2026-01-26 23:06:26 +0100

    2 fase de Agenda de Contactos

--- UD_5/Agenda_de_Contactos/Agenda.java
+++ UD_5/Agenda_de_Contactos/Agenda.java
@@ -1,5 +1,7 @@
 package Agenda_de_Contactos;
 
+import java.util.Scanner;
+
 public class Agenda {
    static final int MAX_size = 100;
     static int count = 0;
@@ -12,10 +14,33 @@ public static void listarContactos(){
         System.out.println("No hay contactos agregados");
     }else{
         for(int i = 0; i < count; i++){
-            System.out.println(contactos[i].toString());
+            System.out.println(i + "." + " " +contactos[i].toString());
         }
     }
 }
+
+
+public static void menuAgenda(){
+
+        System.out.println("======================================");
+        System.out.println("      MENU / AGENDA DE CONTACTOS      ");
+        System.out.println("======================================");
+        System.out.println();
+        System.out.println("1. Listar contactos");
+        System.out.println("2. Agregar contacto");
+        System.out.println("3. Eliminar contacto");
+        System.out.println("4. Buscar por nombre");
+        System.out.println("5. Buscar por el telefono");
+        System.out.println("6. Buscar por el  email");
+        System.out.println("7. Modificar contacto");
+        System.out.println("0. Salir");
+
+}
+
+
+
+
+
 
 public static void agregarContacto(Contacto contacto){
    if (count < MAX_size){
@@ -27,8 +52,62 @@ public static void agregarContacto(Contacto contacto){
 
 }
 
+public static void buscarPorNombre(String nombre){
+    boolean encontrado = false;
+    for(int i = 0; i < count; i++){
+        String nom = contactos[i].getNombre().toLowerCase();
+        if (nom.contains(nombre)){
+            System.out.println(contactos[i].toString());
+            encontrado = true;
+
+        }
+    }
+    if(!encontrado){
+        System.out.println("Contacto no encontrado");
+    }
+}
+    public static void buscarPorTelefono(String telefono){
+        boolean encontrado = false;
+        for(int i = 0; i < count; i++){
+            String tel = contactos[i].getTelefono().toLowerCase();
+            if (tel.contains(telefono)){
+                System.out.println(contactos[i].toString());
+                encontrado = true;
+
+            }
+        }
+        if(!encontrado){
+            System.out.println("Contacto no encontrado");
+        }
+    }
+    public static void buscarPorEmail(String email){
+        boolean encontrado = false;
+        for(int i = 0; i < count; i++){
+            String mail = contactos[i].getEmail().toLowerCase();
+            if (mail.contains(email)){
+                System.out.println(contactos[i].toString());
+                encontrado = true;
+
+            }
+        }
+        if(!encontrado){
+            System.out.println("Contacto no encontrado");
+        }
+    }
+
+public static void eliminarContacto(int num){
+    if(contactos[num] == null){
+        System.out.println("Contacto no existe");
+    }else {
+        for(int i = num; i < count - 1; i++){
+            contactos[i] = contactos[i+1];
+        }
+        contactos[count-1] = null;
+        count--;
+        System.out.println("Contacto eliminado");
 
 
-
+    }
+}
 
 }
--- UD_5/Agenda_de_Contactos/Programa.java
+++ UD_5/Agenda_de_Contactos/Programa.java
@@ -1,23 +1,134 @@
-package Agenda_de_Contactos;
 
+package Agenda_de_Contactos;
 import java.util.Scanner;
 
 public class Programa {
     static Scanner sc = new Scanner(System.in);
-    static void main(String[] args) {
+    static int option;
 
+    public static void main(String[] args) {
+
+        do {
+            Agenda.menuAgenda();
+            option = EligaOption(sc);
+            funcionalidadMenuAgenda(option, sc);
+
+        } while (option != 0);
+    }
+
+
+    public static Contacto pideDatosDeContacto(Scanner sc) {
+        boolean nombreValido = false;
+        String nombre = " ";
+        do {
+
+            System.out.println("Dime el nombre del contacto: ");
+            nombre = sc.nextLine().toLowerCase();
+            if (nombre.length() >= 2) {
+                nombreValido = true;
+            } else {
+                System.out.println("Por favor ingrese un nombre valido");
+            }
+        } while (!nombreValido);
+
+        boolean telefonoValido = false;
+        String telefono = " ";
+        do {
+            System.out.println("Dime el telefono del contacto: ");
+            telefono = sc.nextLine().toLowerCase();
+            if (telefono.length() == 9) {
+                telefonoValido = true;
+            } else {
+                System.out.println("Por favor ingrese un telefono valido");
+            }
+        } while (!telefonoValido);
+
+        boolean emailValido = false;
+        String email = " ";
+        do {
+            System.out.println("Dime el email del contacto: ");
+            email = sc.nextLine().toLowerCase();
+            if (email.contains("@") && email.contains(".")) {
+                emailValido = true;
+            } else {
+                System.out.println("Por favor ingrese un email valido");
+            }
+        } while (!emailValido);
+
+        return new Contacto(nombre, telefono, email);
+    }
+
+
+    static int EligaOption(Scanner sc) {
+        System.out.println();
+        System.out.print("Elige opcion: ");
+        int opcion = Integer.parseInt(sc.nextLine());
+        return opcion;
+    }
+
+
+    public static void funcionalidadMenuAgenda(int option, Scanner sc) {
+        switch (option) {
+            case 1:
+                Agenda.listarContactos();
+
+                break;
+            case 2:
+                Contacto contacto = pideDatosDeContacto(sc);
+                Agenda.agregarContacto(contacto);
+
+
+                break;
+            case 3:
+                eliminarContacto(sc);
+                break;
+            case 4:
+                buscaPorNombre(sc);
+                break;
+            case 5:
+                buscaPorTelefono(sc);
+                break;
+            case 6:
+                buscaPorEmail(sc);
+                break;
+            case 0:
+                break;
+
+
+            default:
+                System.out.println("Opcion no valida");
+                break;
+
+        }
+    }
+
+    public static void buscaPorNombre(Scanner sc) {
         System.out.println("Dime el nombre del contacto: ");
-        String nombre = sc.nextLine();
-        System.out.println("Dime el telefono del contacto: ");
-        String telefono = sc.nextLine();
-        System.out.println("Dime el email del contacto: ");
-        String email = sc.nextLine();
-        Contacto contacto = new Contacto(nombre, telefono, email);
-        Agenda.agregarContacto(contacto);
-        Agenda.listarContactos();
-
-
-
+        String busquedaNombre = sc.nextLine().toLowerCase();
+        Agenda.buscarPorNombre(busquedaNombre);
 
     }
+    public static void buscaPorTelefono(Scanner sc) {
+        System.out.println("Dime el telefono del contacto: ");
+        String busquedaTelefono = sc.nextLine().toLowerCase();
+        Agenda.buscarPorTelefono(busquedaTelefono);
+
+    }
+    public static void buscaPorEmail(Scanner sc) {
+        System.out.println("Dime el email del contacto: ");
+        String busquedaEmail = sc.nextLine().toLowerCase();
+        Agenda.buscarPorEmail(busquedaEmail);
+    }
+
+
+
+    public static void eliminarContacto(Scanner sc) {
+        Agenda.listarContactos();
+        System.out.println("Dime el numero de contacto que desea eliminar: ");
+        int num = Integer.parseInt(sc.nextLine());
+        Agenda.eliminarContacto(num);
+    }
+
+
 }
+
