819e6428867d59158e365690a61a1adc9a64914c
Vladyslav Dumynsky <vladum@alu.edu.gva.es>
2026-04-24 13:53:54 +0200

    :)

--- UD7/Stack/EJ1.java
+++ UD7/Stack/EJ1.java
@@ -19,19 +19,49 @@ public class EJ1 {
 //        for (int i = 0; i < n; i++) {
 //            System.out.println(s.pop());
 //        }
-        System.out.println("Introduce la frase: ");
-        String frase = sc.nextLine();
-        Stack<String> stack = new Stack();
-        String[] array = frase.split(" ");
-        for (int i = 0; i < array.length; i++) {
-            stack.push(array[i]);
+//        System.out.println("Introduce la frase: ");
+//        String frase = sc.nextLine();
+//        Stack<String> stack = new Stack();
+//        String[] array = frase.split(" ");
+//        for (int i = 0; i < array.length; i++) {
+//            stack.push(array[i]);
+//        }
+//
+//        Iterator<String> it = stack.iterator();
+//        while (it.hasNext()) {
+//
+//            System.out.println(stack.pop());
+//
+//        }
+        System.out.println("Introduce exprecion matematica: ");
+        String s =  sc.nextLine();
+        System.out.println(validarStack(s));
         }
 
-        Iterator<String> it = stack.iterator();
-        while (it.hasNext()) {
+        static public Boolean validarStack(String s){
+            Stack<Character> stack = new Stack<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
 
-            System.out.println(stack.pop());
+                if (c == '(' || c == '[') {
+                    stack.push(c);
+                }
+                else if (c == ')' || c == ']') {
+                    if (stack.isEmpty()) return false;
 
+                    char ultimoAbierto = stack.pop();
+                    if ((c == ')' && ultimoAbierto != '(') ||
+                            (c == ']' && ultimoAbierto != '[')) {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.isEmpty();
         }
+
+
+
+
+
     }
-}
